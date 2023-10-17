f96d24e98be2d059c0ef09224241ff2c18a7bb70
Joakim Dangården <Joakim.Dangarden@swegon.com>
2023-10-17 12:34:56 +0200

    Added TabBlazor to docs

---
+++ docs/Linq2GraphQL.Docs/Icons/Icons.cs
@@ -0,0 +1,11 @@
+﻿using TabBlazor;
+
+namespace Linq2GraphQL.Docs
+{
+    public static class Icons
+    {
+        public static IIconType _12_hours => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4' /><path d='M4 13c.468 3.6 3.384 6.546 7 7' /><path d='M18 15h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-1a1 1 0 0 0 -1 1v1a1 1 0 0 0 1 1h2' /><path d='M15 21v-6' />");
+        public static IIconType _123 => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M3 10l2 -2v8' /><path d='M9 8h3a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 0 -1 1v2a1 1 0 0 0 1 1h3' /><path d='M17 8h2.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-1.5h1.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-2.5' />");
+
+    }
+}
--- docs/Linq2GraphQL.Docs/Program.cs
+++ docs/Linq2GraphQL.Docs/Program.cs
@@ -1,11 +1,12 @@
 using Linq2GraphQL.Docs;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TabBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddTabler();
 await builder.Build().RunAsync();
