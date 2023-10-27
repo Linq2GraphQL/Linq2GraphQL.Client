3f0253f81fcb213e642e7712be6983fad3106758
Joakim Dangården <Joakim.Dangarden@swegon.com>
2023-10-27 16:54:01 +0200

    More Docs

---
+++ docs/Linq2GraphQL.Docs/Components/Samples/BasicQuery/BasicQuery.razor.cs
@@ -0,0 +1,18 @@
+using Linq2GraphQL.Client;
+
+namespace Linq2GraphQL.Docs.Components.Samples.BasicQuery
+{
+    public partial class BasicQuery
+    {
+        private GraphQueryExecute<StarWars.Client.FilmsConnection, List<StarWars.Client.Film>> GetQuery()
+        {
+            return starWarsClient
+                .Query
+                .AllFilms(first: 3)
+                .Include(e => e.Films.Select(f => f.Producers))
+                .Select(e => e.Films);
+
+        }
+
+    }
+}
---
+++ docs/Linq2GraphQL.Docs/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Linq2GraphQL.Client;
+
+namespace Linq2GraphQL.Docs
+{
+    public static class Extensions
+    {
+
+        //public static GraphQLSample<T, TResult> GetGraphQLSample<T, TResult>(this GraphQueryExecute<T, TResult> executor)
+        //{
+        //    return new 
+        //}
+
+    }
+}
