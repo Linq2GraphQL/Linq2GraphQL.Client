887e2a47fa9d37668916e33d8c63e2daee7e53e5
magahl <magahl@users.noreply.github.com>
2024-12-16 11:51:34 +0000

    deploy: 860f39d94b1c030398ad30ca8f27fb1f5c51d893

---
+++ _content/razor_samples/Interfaces/InterfaceQuery.razor.cs
@@ -0,0 +1,17 @@
+using Linq2GraphQL.Client;
+using StarWars.Client;
+
+namespace Linq2GraphQL.Docs.Components.Samples.Interfaces
+{
+    public partial class InterfaceQuery
+    {
+        private GraphQueryExecute<Node, Node> GetQuery()
+        {
+            return starWarsClient
+                .Query
+                .Node("ZmlsbXM6MQ==")
+                .Include(e => e.Film())
+                .Select();
+        }
+    }
+}
